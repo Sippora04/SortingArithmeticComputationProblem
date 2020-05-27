b43cc5bd0e72e248d1eb7732ea6d7cab5859ad96
Sippora04 <sippora.toppo95@gmail.com>
2020-05-27 14:26:48 +0530

    StoreResult in Dictionary

--- SortingArithComputation.sh
+++ SortingArithComputation.sh
@@ -8,3 +8,10 @@ var1=$( echo $a $b $c | awk '{print $1+$2*$3}' )
 var2=$( echo $a $b $c | awk '{print $1*$2+$3}' )
 var3=$( echo $c $a $b | awk '{print $1+$2/$3}' )
 var4=$( echo $a $b $c | awk '{print $1%$2+$3}' )
+
+declare -A resultDict
+
+resultDict[0]=$var1
+resultDict[1]=$var2
+resultDict[2]=$var3
+resultDict[3]=$var4
