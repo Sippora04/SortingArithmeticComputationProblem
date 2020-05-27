6cf2aaa634432cce3e269ecd7348c8ddfd037da2
Sippora04 <sippora.toppo95@gmail.com>
2020-05-27 14:15:04 +0530

    calculate arithmetic

--- SortingArithComputation.sh
+++ SortingArithComputation.sh
@@ -4,8 +4,6 @@ read -p "Enter first input: " a
 read -p "Enter second input: " b
 read -p "Enter third input: " c
 
-echo $a $b $c
-
 var1=$( echo $a $b $c | awk '{print $1+$2*$3}' )
+var2=$( echo $a $b $c | awk '{print $1*$2+$3}' )
 
-echo $var1
